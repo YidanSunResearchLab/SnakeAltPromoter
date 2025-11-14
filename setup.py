4c7202c977ad3d9c53ae106cd258928eb8a2792f
Jared Lalmansingh <jared.lalmansingh@wustl.edu>
2025-11-13 22:11:02 -0600

    Patch for conda build which pulls in snakemake-minimal. This has a hard requirement of Python 3.11, which conflicts with the base requirements of snakemake itself. The most direct solution is to only support Python 3.11+, which should be OK as this was released in October 2022.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="SnakeAltPromoter",
-    version="1.0.4",
+    version="1.0.5",
     description="A Snakemake pipeline for alternative promoter analysis",
     author="Yidan Sun",
     author_email="syidan@wustl.edu",
     url="https://github.com/YidanSunResearchLab/SnakeAltPromoter",  # Updated username
     packages=find_packages(),  # Includes workflows/
-    python_requires=">=3.10",  # Bumped to 3.10 for modern support
+    python_requires=">=3.11",  # Bumped to 3.11 for modern support
     include_package_data=True,
     install_requires=[
         "snakemake>=8.28.0",  # Core dependency
@@ -42,7 +42,7 @@ setup(
     },
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10",  # Matches python_requires
+        "Programming Language :: Python :: 3.11",  # Matches python_requires
         "Operating System :: POSIX",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
